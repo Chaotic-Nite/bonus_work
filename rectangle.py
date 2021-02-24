f1c6b80587f0ea7ba362fcd594bcafd075bac098
NoelKling <nitekling@gmail.com>
2021-02-24 12:09:38 -0600

    sorted rectangles by enclosed space and reverse order

--- rectangle.py
+++ rectangle.py
@@ -17,11 +17,14 @@ def enclosed_area(w, h):
 def generate_list(amount):
     tuples_list = []
     while len(tuples_list) < amount:
-        tuples_list.append((random.random(2, 10), random.randint(2, 10)))
+        tuples_list.append((random.randint(2, 9), random.randint(2, 9)))
     return tuples_list
 
 
 def rectangle_sort():
-    # rectangles = generate_list(10)
-    pass
-    
+    rectangles = generate_list(25)
+    rectangles.sort(key=lambda x: enclosed_area(x[0], x[1]), reverse=True)
+    return rectangles
+
+
+print(rectangle_sort())
