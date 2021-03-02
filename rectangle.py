f4308fbc21273b4bfd4c30d62de4393350b66309
NoelKling <nitekling@gmail.com>
2021-03-02 07:39:59 -0600

    Made the list of tuples not random

--- rectangle.py
+++ rectangle.py
@@ -7,22 +7,17 @@ Formula for enclosed area (w-1)*(h-1) - setup for a function to do calculations
 
 __author__ = "ChaoticNite"
 
-import random
-
 
 def enclosed_area(w, h):
     return (w-1)*(h-1)
 
 
-def generate_list(amount):
-    tuples_list = []
-    while len(tuples_list) < amount:
-        tuples_list.append((random.randint(2, 9), random.randint(2, 9)))
-    return tuples_list
+def generate_list():
+    return [(j, i) for j in range(2, 10) for i in range(2, 10)]
 
 
 def rectangle_sort():
-    rectangles = generate_list(25)
+    rectangles = generate_list()
     rectangles.sort(key=lambda x: enclosed_area(x[0], x[1]), reverse=True)
     return rectangles
 
