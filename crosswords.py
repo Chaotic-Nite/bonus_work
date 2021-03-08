cb461a315fa64c7133637c895967ad0826f1819f
NoelKling <nitekling@gmail.com>
2021-03-07 18:51:47 -0600

    Added test strings

---
+++ crosswords.py
@@ -0,0 +1,15 @@
+'''
+Create a function that takes in two strings. Find the intersections of common 
+Formula for enclosed area (w-1)*(h-1) - setup for a function to do calculations
+'''
+
+__author__ = "ChaoticNite"
+
+'''
+Test Strings:
+OIMDIHEIAFNL
+CHJDBJMHPJKD
+'''
+
+def word_sets():
+    pass
